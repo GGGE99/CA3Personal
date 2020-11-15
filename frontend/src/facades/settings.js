b29b8a325d5a75941f4cce866e5d33bbcf5ce3ef
GGGE99 <marc.g.ekstrom@gmail.com>
2020-11-15 15:38:43 +0100

    new iplocattion api

--- frontend/src/facades/settings.js
+++ frontend/src/facades/settings.js
@@ -8,4 +8,4 @@
 export const base = "https://marcge.dk/tomcat/CA3Personal";
 
 export const jokeURL = "https://marcge.dk/tomcat/CA3Personal/api/jokes";
-export const locationURL = "https://api.ipgeolocation.io/ipgeo?apiKey=496ef9715efb448090be97ee10c54df8";
+export const locationURL = "https://ipapi.co/json/";
